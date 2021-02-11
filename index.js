ff1426fec8ab80ba4b696e4d29252ad64021feb0
Leonid S <lenny73brooklyn@gmail.com>
2021-02-11 10:35:36 -0500

    created procfile for heroku

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+const request = require('request');
+
+
+//use body parser middleware
+app.use(bodyParser.urlencoded({extented: false}));
+//API key pk_062031d20883444f9ea74e2610fe2011
+//create call API
+function call_api(finishedAPI, ticker){
+request('https://cloud.iexapis.com/stable/stock/'+ ticker +'/quote?token=pk_062031d20883444f9ea74e2610fe2011',{json:true},(err,res,body)=>{
+		if(err){return console.log(err);}
+		console.log(body);
+		if(res.statusCode ===200){
+			//console.log(body);
+			finishedAPI(body);
+		};
+});
+};
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -12,11 +30,27 @@ app.set('view engine', 'handlebars');
 const otherstuff =" hello there, thid is new stuff!";
 
 app.get('/',function(req,res){
-	res.render('home',{
-		stuff: otherstuff
-
-	});
+	call_api(function(doneAPI){
+			res.render('home',{
+			stock: doneAPI
+		});
+	}, "fb");
 });
+//call_api(function, req.body.stock_ticker)
+app.post('/', function(req,res){
+	call_api(function(doneAPI){
+			//posted_stuff =req.body.stock_ticker;
+			res.render('home',{
+			stock: doneAPI,
+		});
+	}, req.body.stock_ticker);
+});
+
+
+app.get('/about.html',function(req,res){
+	res.render('about');
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
